af2d0207677c51f779f998577ba01322234502a1
John Setiawan <jsetiawan@capterra.com>
2015-10-06 21:42:39 -0400

    More lines.

--- sketch_oct06a.ino
+++ sketch_oct06a.ino
@@ -1,12 +1,22 @@
+const int motion = 13; 
+
 void setup() { 
   Serial.begin(9600);
   
-  for(int i = 2; i < 14; i++) {
+  for(int i = 2; i < 13; i++) {
     pinMode(i, OUTPUT);  
   }
+
+  pinMode(motion, INPUT);
 }
 
 void loop() {  
-  randNumber = random(300);
-  
+  // need 2d array of random number, [[output pin, duration],...]
+  int randNumber = random(300);
+
+  int sensorVal = digitalRead(motion);
+
+  if(motion == HIGH) {
+    
+  }
 }
