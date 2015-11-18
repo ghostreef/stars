e54b90bee58a5563e140a5c3419b6ef90dc9465f
John Setiawan <jsetiawan@capterra.com>
2015-11-18 17:10:11 -0500

    Basic turn lights on and off. Still have to add duration.

--- sketch_oct06a/sketch_oct06a.ino
+++ sketch_oct06a/sketch_oct06a.ino
@@ -1,22 +1,48 @@
 const int motion = 13; 
+int outputs[11];
 
 void setup() { 
   Serial.begin(9600);
   
   for(int i = 2; i < 13; i++) {
     pinMode(i, OUTPUT);  
+    outputs[i - 2] = i;
   }
 
   pinMode(motion, INPUT);
 }
 
 void loop() {  
-  // need 2d array of random number, [[output pin, duration],...]
-  int randNumber = random(300);
+
+  // how many pins to light up
+  int m = random(1, 11);
+
+  // knuth shuffle
+  for(int i = 0; i < m; i++) {
+    // get a new random index from the subset
+    int index = random(0, 10 - i);
+
+    // but the number in the back
+    int temp = outputs[10 - i];
+    outputs[10 - i] = outputs[index];
+    outputs[index] = temp; 
+  }
 
   int sensorVal = digitalRead(motion);
 
-  if(motion == HIGH) {
-    
+  // turn on leds  
+  if(sensorVal == HIGH) {
+    for(int i = 0; i < m; i++) {
+      digitalWrite(outputs[i], HIGH);
+    }
+  }
+
+  delay(2000);
+
+  // turn off leds
+  if(sensorVal == HIGH) {
+    for(int i = 0; i < m; i++) {
+      digitalWrite(outputs[i], LOW);
+    }
   }
 }
